99dcc51ec9219e2bc2fc855d4850ca4451e0b6a1
Sourav <93040364+the-code-breaker@users.noreply.github.com>
2021-11-07 21:49:16 +0530

    Create Number Star pattern 1.py

---
+++ Coding Ninjas/Test 1/Number Star pattern 1.py
@@ -0,0 +1,14 @@
+# Print the following pattern for given number of rows.
+
+lines=int(input()) 
+i=1  
+while(i<=lines):# this loop is used to print lines  
+    j=lines  
+    while (j>=1):# this loop is used to print numbers in a line  
+        if j!=i:  
+            print(j, end='', flush=True)  
+        else:  
+            print('*', end='', flush=True)  
+        j=j-1  
+    print("")  
+    i=i+1;
