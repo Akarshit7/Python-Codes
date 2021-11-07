f831082c7cb4c49c5c726be94dead1403591854d
Sourav <93040364+the-code-breaker@users.noreply.github.com>
2021-11-07 21:01:36 +0530

    Create Arithmetic Progression.py

---
+++ Coding Ninjas/Introduction to Python/Arithmetic Progression.py
@@ -0,0 +1,7 @@
+# You are given first three entries of an arithmetic progression. You have to calculate the common difference and print it.
+
+x = int(input())
+y = int(input())
+z = int(input())
+cd = y - x
+print(cd)
