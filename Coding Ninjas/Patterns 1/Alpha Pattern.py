d954eeea2e2ebebaa2ab6d9f79ece0367fb29276
Sourav <93040364+the-code-breaker@users.noreply.github.com>
2021-11-07 21:32:57 +0530

    Create Alpha Pattern.py

---
+++ Coding Ninjas/Patterns 1/Alpha Pattern.py
@@ -0,0 +1,10 @@
+# Print the following pattern for the given N number of rows.
+#  A
+#  BB
+#  CCC
+
+n=int(input())
+for i in range (65,65+n):
+    for j in range (65,i+1):
+        print(chr(i),end="")
+    print("")
