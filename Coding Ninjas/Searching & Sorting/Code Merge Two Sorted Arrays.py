5f3b061d8b637d4faf079bccc8caa458c347416f
Sourav <93040364+the-code-breaker@users.noreply.github.com>
2021-11-07 23:40:11 +0530

    Create Code Merge Two Sorted Arrays.py

---
+++ Coding Ninjas/Searching & Sorting/Code Merge Two Sorted Arrays.py
@@ -0,0 +1,56 @@
+# You have been given two sorted arrays/lists(ARR1 and ARR2) of size N and M respectively,
+# merge them into a third array/list such that the third array is also sorted.
+
+from sys import stdin
+
+def merge(arr1, n, arr2, m) : 
+    i=0
+    j=0
+    #Your code goes here
+    arr = []
+    while i<n and j<m:
+        if arr1[i]<arr2[j]:
+            arr.append(arr1[i])
+            i+=1
+        else:
+            arr.append(arr2[j])
+            j+=1
+    while i<n:
+        arr.append(arr1[i])
+        i+=1
+    while j<m:
+        arr.append(arr2[j])
+        j+=1
+    return arr 
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(stdin.readline().rstrip())
+    if n != 0:
+        arr = list(map(int, stdin.readline().rstrip().split(" ")))
+        return arr, n
+
+    return list(), 0
+
+
+#to print the array/list
+def printList(arr, n) : 
+    for i in range(n) :
+        print(arr[i], end = " ")
+        
+    print()
+
+
+#main
+t = int(stdin.readline().rstrip())
+
+while t > 0 :
+
+    arr1, n = takeInput()
+    arr2, m = takeInput()
+
+    ans = merge(arr1, n, arr2, m)
+    printList(ans, (n + m))
+
+    t -= 1
