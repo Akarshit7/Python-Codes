247dd0ac4c7d91a5a7fdee4964cde288d1b59d7a
Sourav <93040364+the-code-breaker@users.noreply.github.com>
2021-11-07 20:58:14 +0530

    Create Average Marks.py

---
+++ Coding Ninjas/Introduction to Python/Average Marks.py
@@ -0,0 +1,11 @@
+# Write a program to input marks of three tests of a student (all integers). Then calculate and print the average of all test marks.
+# Code to print average of test marks
+sub_1=int( input())
+
+sub_2 =int(input ())
+
+sub_3 = int(input ())
+
+average= (sub_1+sub_2+sub_3)/3
+
+print(average)
