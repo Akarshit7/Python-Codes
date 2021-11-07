ccf574a05fd1ad399fd4040b0e29095e4d0cf21d
Sourav <93040364+the-code-breaker@users.noreply.github.com>
2021-11-07 21:19:33 +0530

    Create Sum of Even Numbers.py

---
+++ Coding Ninjas/Conditionals and Loops/Sum of Even Numbers.py
@@ -0,0 +1,13 @@
+# Given a number N, print sum of all even numbers from 1 to N.
+
+num = int(input())
+
+total = 0
+
+for i in range(1, num + 1):
+
+    # Check for even or not.
+    if((i % 2) == 0):
+        total = total + i
+
+print(total)
